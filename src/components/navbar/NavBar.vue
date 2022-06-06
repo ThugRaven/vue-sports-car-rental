a59cb6cd287d08099d3cebdd5d0207a51ed8e025
ThugRaven <kamilo1715@wp.pl>
2022-06-06 17:35:20 +0200

    feat: add navbar contact info

--- src/components/navbar/NavBar.vue
+++ src/components/navbar/NavBar.vue
@@ -1,25 +1,53 @@
 <script>
 import NavItem from './NavItem.vue';
+import IconEMail from '../icons/IconEMail.vue';
+import IconPhone from '../icons/IconPhone.vue';
+import IconMenu from '../icons/IconMenu.vue';
 
 export default {
 	components: {
 		NavItem,
+		IconEMail,
+		IconPhone,
+		IconMenu,
 	},
 };
 </script>
 
 <template>
 	<header
-		class="col-start-3 col-end-13 row-span-1 flex items-center justify-between mt-6 ml-0"
+		class="col-start-3 col-end-13 row-span-1 flex items-center justify-end sm:justify-between mt-6 ml-0"
 	>
-		<nav class="">
+		<nav class="hidden sm:block">
 			<ul class="flex flex-row">
 				<NavItem to="/">Strona główna</NavItem>
 				<NavItem to="/cars">Samochody</NavItem>
 				<NavItem to="/contact">Kontakt</NavItem>
 			</ul>
 		</nav>
+		<div
+			class="hidden lg:flex flex-row items-center gap-4 fill-white text-zinc-400 font-semibold"
+		>
+			<span class="flex items-center gap-2"
+				><IconEMail class="w-6 h-6" />car@rent.pl</span
+			>
+			<span class="h-10 w-px bg-zinc-400 mx-1"></span>
+			<span class="flex items-center gap-2 mr-8"
+				><IconPhone class="w-6 h-6" />123 456 789</span
+			>
+
+			<button
+				class="btn hidden xl:block mr-8 py-4 px-10 bg-red-500 hover:bg-red-600 shadow-red-500 rounded-full text-white font-semibold tracking-wide transition hover:scale-105"
+			>
+				Wynajmij teraz
+			</button>
+		</div>
+		<IconMenu class="block sm:hidden w-12 h-12 mx-8 fill-white" />
 	</header>
 </template>
 
-<style></style>
+<style scoped>
+.btn {
+	box-shadow: 0px 10px 60px 5px rgb(239, 68, 68, 0.3);
+}
+</style>
