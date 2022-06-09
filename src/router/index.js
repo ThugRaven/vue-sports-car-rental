e02c633dcbd322d9f335fad36866aa1f14578e69
ThugRaven <kamilo1715@wp.pl>
2022-06-09 23:58:30 +0200

    feat: add dynamic imports

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import CarsView from '../views/CarsView.vue';
-import CarView from '../views/CarView.vue';
 import HomeView from '../views/HomeView.vue';
-import LoginView from '../views/LoginView.vue';
-import RegisterView from '../views/RegisterView.vue';
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,30 +12,22 @@ const router = createRouter({
 		{
 			path: '/cars',
 			name: 'cars',
-			component: CarsView,
+			component: () => import('../views/CarsView.vue'),
 		},
 		{
 			path: '/cars/:id',
 			name: 'car',
-			component: CarView,
+			component: () => import('../views/CarView.vue'),
 		},
 		{
 			path: '/login',
 			name: 'login',
-			component: LoginView,
+			component: () => import('../views/LoginView.vue'),
 		},
 		{
 			path: '/register',
 			name: 'register',
-			component: RegisterView,
-		},
-		{
-			path: '/about',
-			name: 'about',
-			// route level code-splitting
-			// this generates a separate chunk (About.[hash].js) for this route
-			// which is lazy-loaded when the route is visited.
-			component: () => import('../views/AboutView.vue'),
+			component: () => import('../views/RegisterView.vue'),
 		},
 	],
 });
